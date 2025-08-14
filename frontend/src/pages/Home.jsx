a145d93191574ee4960a4fa3ae23a7c73ab85001
JeduthunTheCreator <idemudiajeduthun@gmail.com>
2025-08-14 14:07:28 +0100

    Integrated application routing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,19 @@
 ﻿import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return (
-    <div className="home-page">
-      <h1>All-Purpose Calculator</h1>
-      <p>Welcome to the All-Purpose Calculator! Select a calculation type from the menu to get started.</p>
-    </div>
-  );
+    return (
+        <div className="home-page">
+            <h1>All-Purpose Calculator</h1>
+            <p>Welcome to the All-Purpose Calculator! Select a calculation type from the menu to get started.</p>
+            <div>
+                <ul>
+                  <li><Link to="/scientific">Scientific Calculator</Link></li>
+                  <li><Link to="/binary">Binary Calculator</Link></li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
