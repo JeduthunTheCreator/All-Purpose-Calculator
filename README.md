3fed767c0d1ea8a5b7875cc23784e8fb1348f82b
JeduthunTheCreator <idemudiajeduthun@gmail.com>
2025-08-26 15:27:13 +0100

    Finished designing the homepage

---
+++ README.md
@@ -0,0 +1,12 @@
+## All-Purpose Calculator
+All-purpose Calculator is a full-stack web application that is capable of basic, scientific and binary operations. It is built with **React** and **Vite** for a sleek, innovative and modern user experience. 
+
+## Technologies Used 
+- **Frontend:** React and Vite 
+- **Backend:** Python, Flask, Django, MongoDB 
+- **Styling:** Tailwind CSS / CSS
+- **Animation:** Framer motion
+- **Deployment:** Vercel
+
+---
+<div align="center"> Made with ❤️ by Jeduthun Idemudia </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,31 +5,39 @@ import "../styles/Home.css";
 
 const Home = () => {
     return (
-        <div className="relative min-h-[100dvh] w-full overflow-hidden home-page">
+        <div className="relative min-h-[100dvh] w-full home-page">
             {/* Background animation*/}
             <div className="absolute inset-0 w-full h-full background">
                 <Squares
                     speed={0.5}
                     squareSize={50}
                     direction="diagonal"
-                    borderColor="#000080"
-                    hoverFillColor="#36454F"
+                    borderColor="#271E37"
+                    hoverFillColor="#222222"
                 />
             </div>
 
             {/* Page Content*/}
-            <div className="relative flex flex-col items-center justify-center page-content">
-                <div className="welcome-text text-center">
-                    <h1>All-Purpose Calculator</h1>
-                    <p>Welcome to the All-Purpose Calculator! Select a calculation type from the menu to get started.</p>
+            <div className="relative flex flex-col items-center justify-center min-h-screen page-content">
+
+                <div>
+                    <p className="rounded-full px-[20px] py-[10px] border border-white label"> 🧮 All-Purpose Calculator</p>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 navbar">
-                    <Link to="/scientific">
+                <div>
+                    <p className="text-[30px] text">
+                        Welcome to the All-Purpose Calculator!
+                        <br />
+                        Select a calculation type from the menu to get started.
+                    </p>
+                </div>
+
+                <div className="grid grid-cols-2 navbar">
+                    <Link to="/scientific" className="rounded-full px-[20px] py-[12px] border button">
                         Scientific Calculator
                     </Link>
 
-                    <Link to="/binary">
+                    <Link to="/binary" className="rounded-full px-[20px] py-[12px] border button">
                         Binary Calculator
                     </Link>
                 </div>
