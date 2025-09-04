3fe25febefc38c1c3819cc13ee25f780991323d7
JeduthunTheCreator <idemudiajeduthun@gmail.com>
2025-09-04 20:20:15 +0100

    Integrated Navbar and finished styling Scientific Calculator buttons

--- frontend/src/pages/ScientificCalculator.jsx
+++ frontend/src/pages/ScientificCalculator.jsx
@@ -1,8 +1,11 @@
 ﻿import React, { useState } from 'react';
 import { calculateScientific } from '../services/api';
 import '../styles/ScientificCalculator.css';
-import { FaArrowRightArrowLeft, FaPlusMinus, FaPlus, FaEquals } from 'react-icons/fa6';
+import PillNav from '../components/Navbar.jsx';
+import { FaTimes } from 'react-icons/fa';
+import { FaArrowRightArrowLeft, FaPlusMinus, FaPlus, FaEquals, FaDivide, FaMinus } from 'react-icons/fa6';
 import { IoArrowUndoSharp } from 'react-icons/io5';
+import logo from '../assets/logo(1).webp';
 
 
 const ScientificCalculator = () => {
@@ -11,6 +14,12 @@ const ScientificCalculator = () => {
     const [operation, setOperation] = useState(null);
     const [waitingForNewValue, setWaitingForNewValue] = useState(false);
 
+    // Operator Styling used to update general shared class and each operator class
+    const sharedOperatorStyles = "backdrop-blur-[5.5px] [-webkit-backdrop-filter:blur(5.5px)] border border-white/85 rounded-lg shadow-[0_4px_30px_rgba(35,35,35,0.1)] text-white bg-[#3a4a5c] hover:text-[#232323]";
+    const operatorButtonClass = `calc-button ${sharedOperatorStyles}`;
+    const tripleButtonClass = `calc-button-triple ${sharedOperatorStyles}`;
+
+    const numberButtonClass = ``;
 
     const performOperation = async (nextOperation) => {
         const inputValue = parseFloat(display);
@@ -49,55 +58,50 @@ const ScientificCalculator = () => {
     return (
         <div className="relative flex flex-col items-center justify-center min-h-screen bg-[rgb(6,0,16)] ">
 
+            <h2 className="Page-Header">Scientific Calculator</h2>
+
+            <div className="navigation">
+                <PillNav
+                logo={logo}
+                logoAlt="Company Logo"
+                items={[
+                    { label: 'Home', href: '/' },
+                    { label: 'Binary Calculator', href: '/binary' },
+
+                ]}
+                activeHref="/"
+                className="custom-nav"
+                ease="power2.easeOut"
+                baseColor="#999999"
+                pillColor="#ffffff"
+                hoveredPillTextColor="#ffffff"
+                pillTextColor="#000000"
+                />
+            </div>
+
             <div className="wrapper">
                 <section className="screen">
-                    0
+                    {display}
                 </section>
 
                 <section className="calc-buttons">
                     <div className="calc-button-row">
-                        <button className="calc-button">
-                            2nd
-                        </button>
-                        <button className="calc-button">
-                            π
-                        </button>
-                        <button className="calc-button gap-right">
-                            e
-                        </button>
-                        <button className="calc-button">
-                            [::]
-                        </button>
-                        <button className="calc-button gap-right">
-                            x
-                        </button>
-                        <button className="calc-button">
-                            (
-                        </button>
-                        <button className="calc-button">
-                            ,
-                        </button>
-                        <button className="calc-button gap-right">
-                            )
-                        </button>
-                        <button className="calc-button">
-                            <FaArrowRightArrowLeft/>
-                        </button>
-                        <button className="calc-button">
-                            <IoArrowUndoSharp/>
-                        </button>
+                        <button className="calc-button"> 2nd </button>
+                        <button className="calc-button"> π </button>
+                        <button className="calc-button gap-right"> e </button>
+                        <button className="calc-button"> [::] </button>
+                        <button className="calc-button gap-right"> x </button>
+                        <button className="calc-button"> ( </button>
+                        <button className="calc-button"> , </button>
+                        <button className="calc-button gap-right"> ) </button>
+                        <button className={`${operatorButtonClass} icon-style`} > <FaArrowRightArrowLeft /> </button>
+                        <button className={operatorButtonClass}> <IoArrowUndoSharp /> </button>
                     </div>
 
                     <div className="calc-button-row">
-                        <button className="calc-button">
-                            sin
-                        </button>
-                        <button className="calc-button">
-                            sinh
-                        </button>
-                        <button className="calc-button gap-right">
-                            cot
-                        </button>
+                        <button className="calc-button"> sin </button>
+                        <button className="calc-button"> sinh </button>
+                        <button className="calc-button gap-right"> cot </button>
                         <button className="calc-button">
                             <span className="root-expression">
                                 <span className="root-index">y</span>
@@ -111,34 +115,18 @@ const ScientificCalculator = () => {
                                 <span className="exponent">y</span> 
                             </span>
                         </button>
-                        <button className="calc-button">
-                            7
-                        </button>
-                        <button className="calc-button">
-                            8
-                        </button>
-                        <button className="calc-button gap-right">
-                            9
-                        </button>
-                        <button className="calc-button">
-                            &divide;
-                        </button>
-                        <button className="calc-button">
-                            C
-                        </button>
+                        <button className="calc-button"> 7 </button>
+                        <button className="calc-button"> 8 </button>
+                        <button className="calc-button gap-right"> 9 </button>
+                        <button className={`${operatorButtonClass} icon-style`} > <FaDivide /> </button>
+                        <button className={operatorButtonClass}> C </button>
                     </div>
 
                     <div className="calc-button-row">
+                        <button className="calc-button"> cos </button>
+                        <button className="calc-button"> cosh </button>
+                        <button className="calc-button gap-right"> sec </button>
                         <button className="calc-button">
-                            cos
-                        </button>
-                        <button className="calc-button">
-                            cosh
-                        </button>
-                        <button className="calc-button gap-right">
-                            sec
-                        </button>
-                        <button className="calc-butto">
                             <span className="root-expression">
                                 <span className="root-index">3</span>
                                 <span className="radical">√</span>
@@ -151,30 +139,16 @@ const ScientificCalculator = () => {
                                 <span className="exponent">3</span>
                             </span>
                         </button>
-                        <button className="calc-button">
-                            4
-                        </button>
-                        <button className="calc-button">
-                            5
-                        </button>
-                        <button className="calc-button gap-right">
-                            6
-                        </button>
-                        <button className="calc-button">
-                            &times;
-                        </button>
+                        <button className="calc-button"> 4 </button>
+                        <button className="calc-button"> 5 </button>
+                        <button className="calc-button gap-right" > 6 </button>
+                        <button className={operatorButtonClass}> <FaTimes /> </button>
                     </div>
 
                     <div className="calc-button-row">
-                        <button className="calc-button">
-                            tan
-                        </button>
-                        <button className="calc-button">
-                            tanh
-                        </button>
-                        <button className="calc-button gap-right">
-                            csc
-                        </button>
+                        <button className="calc-button"> tan </button>
+                        <button className="calc-button"> tanh </button>
+                        <button className="calc-button gap-right"> csc </button>
                         <button className="calc-button">
                             <span className="root-expression">
                                 <span className="radical">√</span>
@@ -187,54 +161,28 @@ const ScientificCalculator = () => {
                                 <span className="exponent">2</span>
                             </span>
                         </button>
-                        <button className="calc-button">
-                            1
-                        </button>
-                        <button className="calc-button">
-                            2
-                        </button>
-                        <button className="calc-button gap-right">
-                            3
-                        </button>
-                        <button className="calc-button">
-                            &minus;
-                        </button>
+                        <button className="calc-button"> 1 </button>
+                        <button className="calc-button"> 2 </button>
+                        <button className="calc-button gap-right"> 3 </button>
+                        <button className={operatorButtonClass}> <FaMinus /> </button>
                     </div>
 
                     <div className="calc-button-row">
-                        <button className="calc-button">
-                            ncr
-                        </button>
-                        <button className="calc-button">
-                            npr
-                        </button>
-                        <button className="calc-button gap-right">
-                            %
-                        </button>
-                        <button className="calc-button">
-                            log
-                        </button>
+                        <button className="calc-button"> ncr </button>
+                        <button className="calc-button"> npr </button>
+                        <button className="calc-button gap-right"> % </button>
+                        <button className="calc-button"> log </button>
                         <button className="calc-button gap-right">
                             <span className="square-expression">
                                 <span className="base-10">10</span>
                                 <span className="exponent">x</span>
                             </span>
                         </button>
-                        <button className="calc-button">
-                            0
-                        </button>
-                        <button className="calc-button ">
-                            <FaPlusMinus />
-                        </button>
-                        <button className="calc-button gap-right">
-                            .
-                        </button>
-                        <button className="calc-button">
-                            <FaPlus />
-                        </button>
-                        <button className="calc-button-triple">
-                            <FaEquals />
-                        </button>
+                        <button className="calc-button"> 0 </button>
+                        <button className="calc-button"> <FaPlusMinus /> </button>
+                        <button className="calc-button gap-right"> . </button>
+                        <button className={`${operatorButtonClass} icon-style`}> <FaPlus/> </button>
+                        <button className={`${tripleButtonClass} icon-style`}> <FaEquals /> </button>
                     </div>
                 </section>
             </div>
